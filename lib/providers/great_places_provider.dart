741171c586fabd8b80a5bf2bf9f89f344f51277c
Andrew Marfo <andrewmarfo23@gmail.com>
2024-01-30 22:41:55 +0000

    creating tables

--- lib/providers/great_places_provider.dart
+++ lib/providers/great_places_provider.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 import '../models/place.dart';
+import '../helpers/db_helpers.dart';
 
 class GreatPlacesProvider with ChangeNotifier {
   List<Place> _items = [];
@@ -12,13 +13,21 @@ class GreatPlacesProvider with ChangeNotifier {
   }
 
   void addPlace(String pickedTitle, File pickedImage) {
-    _items.add(
-      Place(
-          id: DateTime.now().toString(),
-          title: pickedTitle,
-          location: null,
-          image: pickedImage),
+    Place newPlace = Place(
+      id: DateTime.now().toString(),
+      title: pickedTitle,
+      location: null,
+      image: pickedImage,
     );
+    _items.add(newPlace);
     notifyListeners();
+    DatabaseHelper.insert(
+      'places',
+      {
+        'id': newPlace.id,
+        'title': newPlace.title,
+        'image': newPlace.image.path,
+      },
+    );
   }
 }
