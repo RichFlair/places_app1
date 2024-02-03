b510bc3a37e9735e72965fb4a0c92f6eae3adde7
Andrew Marfo <andrewmarfo23@gmail.com>
2024-02-03 23:51:26 +0000

    storing and fetching data(fixing bugs)

--- lib/providers/great_places_provider.dart
+++ lib/providers/great_places_provider.dart
@@ -32,7 +32,7 @@ class GreatPlacesProvider with ChangeNotifier {
   }
 
   Future<void> fetchAndGetData() async {
-    var dataBaseItems = await DatabaseHelper.getData('user_places');
+    var dataBaseItems = await DatabaseHelper.getData('places');
     _items = dataBaseItems
         .map(
           (e) => Place(
